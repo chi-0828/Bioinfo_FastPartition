80ac9de0e52d2f3b80ee985991a172bbe9c4d4fa
chi-0828 <gary0828gary@gmail.com>
2021-04-09 17:03:33 +0000

    init

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,16 @@
 #include "intersection_partition.h"
 
 #define PRINT(x) cout<<x<<endl
+void arg(int argc , char**argv);
 
 int main(int argc , char**argv){
+
+    //processing arg
+    arg(argc,argv);
+
+}
+
+void arg(int argc , char**argv){
     cxxopts::Options options("quickphasing", "Itersection pahsing");
     options.add_options()
     ("r,reference", "File name", cxxopts::value<std::string>())
@@ -39,22 +47,21 @@ int main(int argc , char**argv){
     if (result.count("bam"))
         bam = result["bam"].as<string>();
     else{
-        PRINT("Please input bam file");
+        PRINT("Please input bam file path");
         exit(1);
     }
     if (result.count("vcf"))
         vcf = result["vcf"].as<string>();
     else{
-        PRINT("Please input vcf file");
+        PRINT("Please input vcf file path");
         exit(1);
     }
     if (result.count("output"))
         outputfile = result["output"].as<string>();
     else{
-        PRINT("Please input output file");
+        PRINT("Please input output file path");
         exit(1);
     }
 
     cout<<reference<<bam<<vcf<<endl;
-    //start()
 }
